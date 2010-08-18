dec2e972cc0c85241bfa75243835671d1420e6b1
Ben Griffiths <bengriffiths@gmail.com>
2010-08-18 01:25:45 +0100

    Tidied up javascript

---
+++ govspark_api.js
@@ -0,0 +1,66 @@
+Array.prototype.sum = function() {
+    var s = 0.0;
+    for (var i = 0; i < this.length; i++) {
+      s += (typeof this[i] == 'number') ? this[i] : parseFloat(this[i]);
+    }
+    return s;
+};
+
+Array.prototype.mean = function() {
+    return (this.sum() / this.length)
+}
+
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice(to + 1);
+  return this.slice(0,from).concat(rest);
+};
+
+function request_data(name,callback) {        
+    var url = "http://govspark-api-test-v2.appspot.com/json/"+name;       
+    url += "?callback=?"
+    jQuery.getJSON(url,callback);
+}
+
+function data_for(jsonDoc) {
+    var howManyDays = jsonDoc.length;
+	var name = "";
+
+    var dailyUsage = [];
+    var dailyUsageWh = [];
+    var dailyUsageNwh = [];
+    
+    for (var i = 0; i < howManyDays; i++) {
+
+        var date = jsonDoc[i]["Date"];
+        name = jsonDoc[i]["name"];
+        
+        var readings =  jsonDoc[i]['readings'];
+        var workingHours = readings.slice(18,38);
+        var nonWorkingHours = readings.remove(18,37); 
+        
+        console.log(readings);
+        console.log(workingHours.length);
+        console.log(nonWorkingHours.length);
+        
+		dailyUsage.push(readings.sum());
+        dailyUsageWh.push(workingHours.sum());
+        dailyUsageNwh.push(nonWorkingHours.sum());        
+    }
+
+	var all_dayMean = dailyUsage.mean() / 24;
+	var all_workMean = dailyUsageWh.mean() / 10;
+	var all_otherMean = dailyUsageNwh.mean() / 14;
+	var all_reduction = 100 * ( 1.0 - all_otherMean/all_workMean);
+	
+	return {
+	  "date": date,
+	  "name": name,
+	  "dailyUsage": dailyUsage,
+	  "dailyUsage-working": dailyUsageWh,
+	  "dailyUsage-non-working": dailyUsageNwh,
+	  "averageUsage": all_dayMean,
+	  "averageUsage-working": all_workMean,
+	  "averageUsage-non-working": all_otherMean,
+	  "pctLessOvernight": all_reduction
+	};
+}
---
+++ table.js
@@ -0,0 +1,111 @@
+google.load("visualization", "1", {packages:["imagesparkline","gauge"]});
+google.setOnLoadCallback(createTable);
+
+Number.prototype.commaSeparated = function() {
+    value = Math.round(this).toString();
+    var regexp = new RegExp('(-?[0-9]+)([0-9]{3})');
+    while(regexp.test(value)) {
+        value = value.replace(regexp, '$1,$2');
+    }
+    return value;
+}
+
+function tableRow(parent) {
+    var row = document.createElement('tr');
+    parent.appendChild(row);
+    return {
+        rowElement: row,
+        
+        createTableRowContent : function (data, cellType,id,cssClass) {
+            var rowContent = document.createElement(cellType);
+            var cell;
+            
+            if (cssClass)
+                rowContent.className = cssClass;
+                
+            if (id) {
+                cell = document.createElement("div");
+                cell.id = id;
+            } else {
+                cell = document.createTextNode(data);
+            }
+            rowContent.appendChild(cell);
+            this.rowElement.appendChild(rowContent);
+        },
+        
+        cell: function(data,id,cssClass) {
+            this.createTableRowContent(data, 'td',id,cssClass);
+        },
+        
+        header: function(data,id) {
+            this.createTableRowContent(data, 'th',id);
+        },
+        
+        headers: function(headers) {
+            for (var i=0; i < headers.length; i++) {
+                this.header(headers[i]);
+            };
+        },
+        
+        cells: function(cellValues,classes) {
+            for (var i=0; i < cellValues.length; i++) {
+                if (typeof cellValues[i] == 'object')
+                    this.cell(cellValues[i][0],cellValues[i][1],classes[i]);
+                else
+                    this.cell(cellValues[i],null,classes[i]);
+            }
+        }
+    };
+}
+
+function createTable() {
+    
+    var table = document.createElement('table');
+    var thead = document.createElement('thead');
+    var tbody = document.createElement('tbody');
+    
+    document.getElementById('mytable').appendChild(table);
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    
+    var row = tableRow(thead);
+    row.headers(['Department','Weekly Usage','Site Name','']);
+        
+    var depts = ["hmt","co","dcms","decc","defra","fco","hmrc"];
+    for(var j = 0; j < depts.length; j++) {
+        request_data(depts[j], function(jsonDoc) {
+            populateRow(tableRow(tbody),data_for(jsonDoc));
+        });
+    }
+}
+
+function populateRow(row,usage) {
+
+    var text = 'This department\'s HQ uses an average of  '+
+                usage['averageUsage'].commaSeparated() +
+               ' kWh daily, and, typically, ' + 
+                usage['pctLessOvernight'].commaSeparated() + 
+               '% less energy overnight than it does during the day.';
+    
+    var name = usage['name'];
+    var chart_id = 'chart-'+name.replace(' ','');
+
+    row.cells([name,[chart_id,chart_id],name,text],['dept','','address']);
+    sparkline(usage['dailyUsage'],chart_id)
+}
+
+function sparkline(usage,dest_id) {
+    var dest = document.getElementById(dest_id);
+    var data = new google.visualization.DataTable();
+    
+    data.addColumn("number", "kWh");
+    data.addRows(usage.length);
+    for(var j = 0; j < usage.length; j++) {
+        data.setValue(j,0,usage[j]);
+    }
+    
+    var chart = new google.visualization.ImageSparkLine(dest);
+    chart.draw(data, {width: 120, height: 40, showAxisLines: false,  showValueLabels: false, labelPosition: 'none'});
+}
+
+
